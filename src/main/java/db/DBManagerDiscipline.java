61bbd4e49c0ed54a5a587bc201115147e1fcfe3b
OlegKoc <koralll6@yandex.ru>
2023-03-09 20:37:17 +0400

    added classes DBManagerDiscipline, fixed errors in the class DB ManagerCintroller

--- src/main/java/db/DBManagerDiscipline.java
+++ src/main/java/db/DBManagerDiscipline.java
@@ -1,6 +1,54 @@
 package db;
 
+import entity.Discipline;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DBManagerDiscipline {
 
 
+    private static Statement statement;
+    private static final String ID = "id";
+    private static final String DISCIPLINE = "discipline";
+
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //com.mysql.cj.jdbc.Driver
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/nwe_students?user=root&password=vkznm2000"); // соединение с   БД
+            statement = connection.createStatement(); // объект для отправки запросов в БД
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public static List<Discipline> getAllActiveDiscipline() {
+        List<Discipline> disciplines = new ArrayList<>();
+        try {
+
+            ResultSet resultSet = statement.executeQuery("SELECT d.id, discipline from discipline as d"); //запрос в БД
+
+            while (resultSet.next()) {             //создем объект класса Discipline, проходим циклом по каждой дисциплине и сохраняем в переменную
+
+                Discipline discipline = new Discipline();
+
+                discipline.setID(resultSet.getInt(ID));
+                discipline.setName(resultSet.getString(DISCIPLINE));
+
+                disciplines.add(discipline);    // добавляем все дисциплины из объекта discipline  в коллекцию Discipline  и возвращаем  уже заполненную коллекцию
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return disciplines;
+
+
+    }
 }
--- src/main/webapp/WEB-INF/jsp/discipline.jsp
+++ src/main/webapp/WEB-INF/jsp/discipline.jsp
@@ -1,4 +1,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="f" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,39 +15,34 @@
     <title>Disciplines List</title>
 </head>
 <body>
-<button><a href="index.html" > на главную</a></button>
+<button><a href="/" > на главную</a></button>
 <h2 class="text-shadow-2" align="center">Дисциплины</h2>
 <ul><a href="Modifying1.html"><button href="" class="gradient-button">Создание/изменение дисциплины</button></a></ul>
 
 <table border="1">
     <tbody>
-    <tr>
-        <th></th>
+       <tr>
+           <th></th>
 
 
-        <th>ID</th>
-        <th>Дисциплина</th>
-        <th>Статус</th>
-    </tr>
-    <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
+           <th>ID</th>
+           <th>Дисциплина</th>
+
+           <c:forEach  items = "${discipline}" var="s">
+                 <tr>
+                    <td><input type="checkbox"></td>
+                    <td>${s.ID}</td>
+                    <td>${s.name}</td>
+
+                 </tr>
+          </c:forEach>
+
+
+
+
 
     </tr>
-    <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-    </tr>
-    <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-    </tr>
+
     </tbody>
 
 </table>
